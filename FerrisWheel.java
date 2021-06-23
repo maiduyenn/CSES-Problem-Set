b1a36989d52e106c5ff1ac44448f0eedd713177f
Nguyễn Thị Mai Duyên <77086538+maiduyenn@users.noreply.github.com>
2021-06-23 09:10:40 +0700

    Ferris Wheel - CSES Problem Set
    Ferris Wheel - CSES Problem Set link: https://cses.fi/problemset/task/1090

---
+++ FerrisWheel.java
@@ -0,0 +1,133 @@
+import java.util.*;
+import java.io.*;
+
+public class FerrisWheel {
+
+	static InputStream is = System.in;
+
+	public static void main(String[] args) {
+		int n = ni();
+		long maxWeight = nl();
+		List<Long> weight = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			weight.add(nl());
+		}
+		Collections.sort(weight);
+		int count = 0;
+		for (int i = 0, j = n - 1; i <= j && j >= i;) {
+			if (i == j) {
+				count++;
+				break;
+			}
+			if (weight.get(i) == maxWeight) {
+				count++;
+				i++;
+			} else if (weight.get(i) + weight.get(j) > maxWeight) {
+				j--;
+				count++;
+			} else {
+				count++;
+				i++;
+				j--;
+			}
+		}
+		System.out.println(count);
+	}
+
+	static byte[] inbuf = new byte[1 << 24];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+}
