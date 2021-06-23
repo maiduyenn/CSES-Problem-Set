5847e3b1cd0276aaf58f5707195c83f98fd6d2fd
Nguyễn Thị Mai Duyên <77086538+maiduyenn@users.noreply.github.com>
2021-06-23 11:45:19 +0700

    Dice Combinations - CSES Problem Set
    Dice Combinations - CSES Problem Set link: https://cses.fi/problemset/task/1633/

---
+++ DiceCombinations.java
@@ -0,0 +1,129 @@
+import java.util.*;
+
+import java.io.*;
+
+public class DiceCombinations {
+
+	static InputStream is = System.in;
+
+	public static void main(String[] args) {
+		int n = ni();
+		List<Long> dice = new ArrayList<Long>();
+		dice.add((long) 1);
+		dice.add((long) 1);
+
+		for (int i = 2; i < 7; i++) {
+			long sum = 0;
+			for (int j = 0; j < i; j++) {
+				sum += dice.get(j);
+			}
+			dice.add(sum);
+		}
+		long sum = 0;
+		for (int i = 7; i <= n; i++) {
+			sum = (dice.get(i - 1) + dice.get(i - 2) + dice.get(i - 3) + dice.get(i - 4) + dice.get(i - 5)
+					+ dice.get(i - 6)) % 1000000007;
+			dice.add(sum);
+		}
+
+		System.out.println(dice.get(n));
+
+	}
+
+	static byte[] inbuf = new byte[1 << 24];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+}
