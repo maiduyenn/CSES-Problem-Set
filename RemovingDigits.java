af0b3870cbe344aa68113fd0aef979d1a813bdd0
Nguyễn Thị Mai Duyên <77086538+maiduyenn@users.noreply.github.com>
2021-06-30 13:06:01 +0700

    Add files via upload

---
+++ RemovingDigits.java
@@ -0,0 +1,129 @@
+import java.util.*;
+
+import java.io.*;
+
+public class RemovingDigits {
+
+	static InputStream is = System.in;
+	static int count = 0;
+
+	public static void main(String[] args) {
+		int number = ni();
+		remove(number);
+		System.out.println(count);
+	}
+
+	static void remove(int number) {
+		while (number > 0) {
+			number -= max(number);
+			count++;
+		}
+	}
+
+	static int max(int n) {
+		int max = 0;
+		while (n > 0) {
+			int digit = n % 10;
+			max = Math.max(max, digit);
+			n /= 10;
+		}
+		return max;
+	}
+
+	static byte[] inbuf = new byte[1 << 24];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+}
